da0dd82ffc74485763f23ecb361f632389a6ae99
hritish-mahajan <hhritish_be22@thapar.edu>
2026-09-15 11:51:35 +0530

    check

---
+++ first/src/App11.java
@@ -0,0 +1,21 @@
+public class App11 {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+
+        //creating a class and learning how it functions.
+        //revision
+        /* 
+        The main method does not require creating an object since it 
+        is executed by the JVM.
+
+        public -> access modifier
+        static -> a keyword; allows calling a method without creating an obj
+        void -> return type; 
+        main -> method name; the entry point of the program
+        clearly a method cannot be abstract and static at the same time.
+        */
+        
+
+
+    }
+}
